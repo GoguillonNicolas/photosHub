37830e6185dd826b8d4f25711336008187b1bd90
Nicolas Goguillon <nicolas.goguillon59@gmail.com>
2025-12-08 18:13:13 +0100

    feat(login): adding a login form

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
-from photohub import app
+from photohub import app, db
+import click
+
+@app.cli.command("create-db")
+def create_db():
+    """Crée les tables de la base de données."""
+    with app.app_context():
+        db.create_all()
+    click.echo("Base de données initialisée.")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
